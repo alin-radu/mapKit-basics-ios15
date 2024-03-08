ddbbe81d0395ed4d8908a6239577da8aa5b31599
Alin RADU <alin.radu.cristian@gmail.com>
2024-03-08 12:14:02 +0200

    Initial Commit

---
+++ MapKitBasics/MapKitBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitBasicsApp.swift
+//  MapKitBasics
+//
+//  Created by Alin RADU on 08.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapKitBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
